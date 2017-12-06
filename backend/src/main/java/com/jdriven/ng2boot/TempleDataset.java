1fa7160745d4fcb99ec6755c450b6c316b37d66b
Gaurav Raje <gaurav.raje07@gmail.com>
2017-12-05 22:38:12 -0500

    Merge remote-tracking branch 'chimbstockangular/master' into dlf4j

---
+++ backend/src/main/java/com/jdriven/ng2boot/TempleDataset.java
@@ -0,0 +1,15 @@
+package com.jdriven.ng2boot;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
+
+/**
+ * Created by chimbs on 12/5/17.
+ */
+public class TempleDataset {
+    public static void main(String[] args) throws IOException {
+        String st  = FileUtils.readFileToString(new java.io.File("/Users/chimbs/chimbstockangular/backend/src/main/resources/5Neg5PosSentences.txt"));
+        int a =1 ;
+    }
+}
